8648e4c719386fe4ed3d36fb0dceb0392c5f6891
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-04-13 15:31:36 +0300

    Update test_netbox.py

--- test_netbox.py
+++ test_netbox.py
@@ -1,12 +1,42 @@
-#!/usr/bin/python3
+# Modules
+from icmplib import ping
 
-import pynetbox
-URL = 'http://192.168.85.128/'
-API_KEY = 'e89ec975b68720dc0ca9f2cab84f9ab546d4755e'
-netbox = pynetbox.api(URL, token = API_KEY)
 
-response_list = netbox.dcim.interfaces.filter(device='R1')
-test_list = []
-for i in response_list:
-    test_list.append(i)
-print(test_list)
+# Netbox artifcats
+from extras.scripts import Script, ObjectVar, MultiObjectVar
+from dcim.models import Site, Device
+
+
+# Classes
+class DeviceChecker(Script):
+    class Meta:
+        name = "Validate state of network devices"
+        description = "Sample Script to set the status of the device based on its availability"
+        field_order = ("site", "devices")
+
+    site = ObjectVar(
+        model=Site
+    )
+
+    devices = MultiObjectVar(
+        model=Device,
+        query_params={
+            "site_id": "$site"
+        }
+    )
+
+    def run(self, data, commit) -> None:
+        print_in_nb = []
+        for device in data["devices"]:
+            result = ping(str(device.primary_ip.address.ip), count=3, interval=0.2, privileged=False)
+       
+            if result.is_alive:
+                device.status = "active"
+
+            else:
+                device.status = "offline"
+
+            print_in_nb.append(f"{device.name} is set to {device.status}")
+            device.save()
+
+        return "\n".join(print_in_nb)
