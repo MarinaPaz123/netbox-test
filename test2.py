58925e1b06efe28c1a7a5a1d2fbe5d12e9f897c0
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-03 16:26:29 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 # Modules
 from icmplib import ping
-
+from netmiko import ConnectHandler
 
 # Netbox artifcats
 from extras.scripts import Script, ObjectVar, MultiObjectVar
@@ -22,8 +22,24 @@ class DeviceChecker(Script):
         model=Device,
         query_params={"role_id": "$devices_role"}
 
-
-
     )
+
+    def run(self, data, commit) -> None:
+        print_in_nb = []
+        for device in data["devices"]:
+            cisco_dev = {
+            'device_type': 'cisco_ios',
+            'host': str(device.primary_ip.address.ip),
+            'username': 'cisco',
+            'password': 'cisco',
+            'secret': 'cisco',
+            }
+            connection = ConnectHandler(**cisco_dev)
+            connection.enable()
+            result = connection.send_command('show ip int br')
+            print_in_nb.append(result)
+
+            device.save()
+        return "\n".join(print_in_nb)
 
 
