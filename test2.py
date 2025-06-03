9228e4facb6e2565a13a2f60f2e72fb3b13ec6f9
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-03 09:22:34 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,44 +1,25 @@
 # Modules
+# Modules
 from icmplib import ping
 
 
 # Netbox artifcats
 from extras.scripts import Script, ObjectVar, MultiObjectVar
-from dcim.models import Site, Device
+from dcim.models import DeviceRole, Device
 
 
 # Classes
 class DeviceChecker(Script):
     class Meta:
-        name = "Псс,брат.Проверь статус устройств"
+        name = "Псс"
         description = "Sample Script to set the status of the device based on its availability"
-        field_order = ("site","device roles", "devices")
+        field_order = ("deviceroles", "devices")
 
-    site = ObjectVar(
-        model=Site
+    devrole = ObjectVar(
+        model=DeviceRole
     )
 
-    #device_roles =
-
-    devices = MultiObjectVar(
+    dev = MultiObjectVar(
         model=Device,
-        query_params={
-            "site_id": "$site"
-        }
     )
 
-    def run(self, data, commit) -> None:
-        print_in_nb = []
-        for device in data["devices"]:
-            result = ping(str(device.primary_ip.address.ip), count=3, interval=0.2, privileged=False)
-
-            if result.is_alive:
-                device.status = "active"
-
-            else:
-                device.status = "offline"
-
-            print_in_nb.append(f"{device.name}  ёбта {device.status}")
-            device.save()
-
-        return "\n".join(print_in_nb)
