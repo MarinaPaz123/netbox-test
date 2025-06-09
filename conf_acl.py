259f4879b9ff83f9b43c1bccc8e8ac058664cc32
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-09 07:48:22 +0300

    Create conf_acl.py

---
+++ conf_acl.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment, FileSystemLoader
+import yaml
+
+from extras.scripts import Script, ObjectVar, MultiObjectVar, TextVar, ChoiceVar
+
+class ConfACL(Script):
+    class Meta:
+        name = "Игра: собери свой ACL,бро"
+        description = "conf_ACL"
+
+        devices_role = ObjectVar(
+            model=DeviceRole,
+            context= "Выбери роль устройства,э блэт"
+            )
