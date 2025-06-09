a0c0d8f0e7b9477a62604cab8ae04c285441158b
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-09 18:03:27 +0300

    Update conf_acl.py

--- conf_acl.py
+++ conf_acl.py
@@ -11,7 +11,6 @@ class ConfACL(Script):
 
     devices_role = ObjectVar(
         model=DeviceRole,
-        context= "Выбери роль,э"
         )
     def run(self, data, commit) -> None:
         pass
