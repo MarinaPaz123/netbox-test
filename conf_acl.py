99e65e7c634aece3ea060770f62a3fdcc3abbaf7
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-10 10:50:07 +0300

    Update conf_acl.py

--- conf_acl.py
+++ conf_acl.py
@@ -1,6 +1,7 @@
 from dcim.models import DeviceRole, Device
 from dcim.models.mixins import RenderConfigMixin
 from extras.scripts import Script, ObjectVar, MultiObjectVar, TextVar, ChoiceVar
+from dcim.models.devices import DeviceRole
 
 
 from jinja2 import Environment, FileSystemLoader
@@ -17,8 +18,8 @@ class ConfACL(Script):
         model=DeviceRole,
         )
     
-    #template_dev = RenderConfigMixin()
-
+    template_dev = 
+    
     def run(self, data, commit) -> None:
         pass
 
