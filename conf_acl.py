7b3941bdff68cd67ab2d7759aaadff9336f10c36
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-12 11:38:22 +0300

    Update conf_acl.py

--- conf_acl.py
+++ conf_acl.py
@@ -2,7 +2,7 @@ from dcim.models import DeviceRole, Device
 from dcim.models.mixins import RenderConfigMixin
 from extras.scripts import Script, ObjectVar, MultiObjectVar, TextVar, ChoiceVar, FileVar
 from dcim.models.devices import DeviceRole
-from django.utils.text import slugify
+#from django.utils.text import slugify
 
 
 from jinja2 import Environment, FileSystemLoader
@@ -23,7 +23,7 @@ class ConfACL(Script):
     def run(self, data, commit) -> None:
         test_list = []
         for dev in data["devices"]:
-            test_list.append(str(dev.custom_fields))
+            test_list.append(str(dev.custom_fields.CustomField))
          
         return test_list
             
