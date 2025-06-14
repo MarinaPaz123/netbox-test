b0c7b47c99c8ffe477fc9e9543d03aceae091017
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-14 10:13:01 +0300

    Update conf_access_sw.py

--- conf_access_sw.py
+++ conf_access_sw.py
@@ -1,1 +1,16 @@
+from extras.scripts import Script, ObjectVar, MultiObjectVar, TextVar
+from dcim.models import DeviceRole, Device
+
+class Access_sw(Script):
+  class Meta:
+    name = "Настрой свитч доступа"
+    description = "conf access sw"
+    
+  devices = MultiObjectVar(
+        description = "Выбери устройство",
+        model=Device.role,
+        )
+
+  def run(self, data, commit) -> None:
+    pass
 
