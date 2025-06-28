e66d105dfeba28f1b4b2de283045bfac8ca898a6
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-28 20:36:12 +0300

    Update conf_access_sw.py

--- conf_access_sw.py
+++ conf_access_sw.py
@@ -24,7 +24,7 @@ class Access_sw(Script):
     description='какая-то херня',
     )
   clone_fields = (
-        'action',"site"
+        'scope_type','scope_id','action',"site"
     )
   def clean(self):
     super().clean()
