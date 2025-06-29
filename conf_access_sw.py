59231cbe1ec9f3809c924df79d8c81ca6aff3c41
MarinaPaz123 <150282987+MarinaPaz123@users.noreply.github.com>
2025-06-29 09:38:18 +0300

    Update conf_access_sw.py

--- conf_access_sw.py
+++ conf_access_sw.py
@@ -17,7 +17,8 @@ class Access_sw(Script):
   
   vlan_id = MultiObjectVar(
        description = "Выбери vlan,братишка",
-       model=VLAN)
+       model=VLAN,
+       query_params={"site_id": 1} # Выбрать можно vlan только в сайте "test"!)
         
 
 
